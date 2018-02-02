d4d94381bb077ec764cff7fa3fa2dddfde232fe1
Chris Ye <chrisyeshi@gmail.com>
2018-02-02 12:15:31 -0800

    fixed indentations.

--- functional.h
+++ functional.h
@@ -11,28 +11,28 @@ template <typename Type, typename Transform>
 auto map(const std::vector<Type>& input, Transform transform)
 		-> std::vector<decltype(transform(input[0]))> {
     std::vector<decltype(transform(input[0]))> output(input.size());
-	for (unsigned int i = 0; i < input.size(); ++i) {
-		output[i] = transform(input[i]);
-	}
-	return output;
+    for (unsigned int i = 0; i < input.size(); ++i) {
+            output[i] = transform(input[i]);
+    }
+    return output;
 }
 
 template <typename Type, typename Evaluate>
 std::vector<Type> filter(const std::vector<Type>& input, Evaluate evaluate) {
-	std::vector<Type> output;
-	for (const auto& entry : input) {
-		if (evaluate(entry)) {
-			output.push_back(entry);
-		}
-	}
-	return output;
+    std::vector<Type> output;
+    for (const auto& entry : input) {
+            if (evaluate(entry)) {
+                    output.push_back(entry);
+            }
+    }
+    return output;
 }
 
 template <typename Type>
 std::vector<Type> sort(const std::vector<Type>& input) {
-	std::vector<Type> output = input;
-	std::sort(output.begin(), output.end());
-	return output;
+    std::vector<Type> output = input;
+    std::sort(output.begin(), output.end());
+    return output;
 }
 
 } // namespace fp
